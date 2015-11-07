d022068e557fbb4780d5811a1114f5d393671b4e
David Cuadra Quevedo <dcuadraq@gmail.com>
2015-11-07 03:28:59 -0600

    Add some helpers for models

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,24 @@ class Event < ActiveRecord::Base
   has_many :assistants
 
   validates :name, presence: true
+
+  def earliest_possible_assistant
+    earliest_time = nil
+    assistants.each do |assistant|
+      earliest_time = assistant.earliest_availability if assistant.earliest_availability < earliest_time
+    end
+    earliest_time
+  end
+
+  def latest_possible_assistant
+    latest_time = nil
+    assistants.each do |assistant|
+      latest_time = assistant.latest_availability if assistant.latest_availability < latest_time
+    end
+    latest_time
+  end
+
+  def initial_times_for_assistants
+    assistants.map(&:initial_times)
+  end
 end
